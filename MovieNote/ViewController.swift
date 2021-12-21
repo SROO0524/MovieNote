395969aefe24aec1204b3bfef32fc2be7b4f5d0e
della <della@herren.co.kr>
2021-12-21 21:52:02 +0900

    Initial Commit

---
+++ MovieNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieNote
+//
+//  Created by 김믿음 on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
